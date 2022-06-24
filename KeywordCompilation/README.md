1cb0faede221cba296f61646c62a25f262324218
Akhil Ennu <akhil.e2012@gmail.com>
2022-06-24 14:07:26 +0530

    Keyword compilation

---
+++ KeywordCompilation/README.md
@@ -0,0 +1,8 @@
+#Keyword Compilation 
+
+This project needs a video file, a subtitle file and a keyword.
+
+All the subtitles that contain the keyword will be collected and the start and end time of that subtitle will be collected.
+This start and end time will be used to extract a clip from the video that contains the video.
+
+Compilation of all the clips in the video file that contain the keyword will be created and exported as a video.
---
+++ KeywordCompilation/movie_keyword_complilation.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 23 18:55:47 2022
+
+@author: mvina
+"""
+import moviepy
+from moviepy.editor import *
+import re
+import sys
+import time
+
+
+prog_start_time = time.time()
+movie_name = str(sys.argv[1])
+subtitle = str(sys.argv[2])
+keyword = str(sys.argv[3])
+times=[]
+openFile = open(subtitle,'r')
+readFile = openFile.readlines()
+previousLine =""
+for line in readFile:
+    if keyword in line:
+        #print(line)
+        if previousLine[:1].isdigit():
+            lines = re.split(r"-->|,",re.sub(" ","",previousLine))
+            times.append((lines[0],lines[2]))
+    previousLine=line
+#print(times)
+openFile.close()
+
+clips = []
+clip = VideoFileClip(movie_name)
+for value in times:
+    start_time = sum(x * int(t) for x, t in zip([3600, 60, 1], value[0].split(":"))) 
+    end_time = sum(x * int(t) for x, t in zip([3600, 60, 1], value[1].split(":")))
+    #print("start time = "+str(start_time)+" end time = "+str(end_time))
+    clip_cut = clip.subclip(start_time, end_time)
+    clips.append(clip_cut)
+    
+final = concatenate_videoclips(clips) 
+final.write_videofile("compiled.mp4")
+#ffmpeg_extract_subclip(movie_name, 20, 30, targetname="compiled.mp4")
+
+
+prog_end_time = time.time()
+
+print("Time taken to create the file is "+str(round((prog_end_time-prog_start_time),2)))
